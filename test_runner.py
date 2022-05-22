077299dde5b2086df95d5eaa04f86e5e9f8d97e6
apamoori <apamoori@oisit-ist24215.unomaha.edu>
2022-05-21 22:33:31 -0500

    improve test_runner

--- test_runner.py
+++ test_runner.py
@@ -9,18 +9,14 @@ def get_time_value(cmd):
 
     result = subprocess.check_output(cmd,shell=True).decode('utf-8')
     lines = result.splitlines()
+    # print(result)
 
     find1 = result.find('SATISFIABLE')
     find2 = result.find('UNSATISFIABLE')
 
-    # print(result)
-    
-    
-    # print(result)
-
     if(args.cputime):
         filtered = filter(lambda x: x.find("#   total CPU time (s): ")!=-1, lines)
-        time = "CPU TIME"
+        time_type = "CPU TIME"
         val = list(filtered)[0].split(" ")[-1]
     
     else:
@@ -28,6 +24,8 @@ def get_time_value(cmd):
         time_type = "USER TIME"
         val = list(filtered)[0].split(" ")[-1]
 
+    if(find2==-2):
+        return (time_type,-2)
     if(find2!=-1 or find1==-1):
         return (time_type, -1)
 
@@ -40,10 +38,6 @@ if __name__ == '__main__':
     parser.add_argument("-u", "--usertime", action="store_true", help="show user time")
     parser.add_argument("-c", "--cputime", action="store_true", help="show cpu time")
     args = parser.parse_args()
-
-    # subprocess.run(["./runsolver","--timestamp", "-C 1200", "-W 180", "-R 8192", "-V 102400", "-w watch.log", "-v values.log", "-o tool_output.log","python /home/ubuntu/asp/old/eclingo/eclingo/main.py", "-c length=9 0 /home/ubuntu/asp/old/eclingo/test/yale/yale.lp /home/ubuntu/asp/old/eclingo/test/yale/input/yale01.lp"])
-    # os.system("./runsolver --timestamp -w newclingo_watch2.log -v newclingo_values2.log -o newclingo_tool_output2.log python eclingo/eclingo/main.py -c length=9 0 eclingo/test/yale/yale.lp eclingo/test/yale/input/yale02.lp")
-    
     
     commands = ["./runsolver --timestamp python eclingo/main.py -c length=9 0 test/yale/yale.lp test/yale/input/yale01.lp"]
 
@@ -73,16 +67,14 @@ if __name__ == '__main__':
 
         if(val==-1):
             print("error in file:",instance)
+            results.append([instance,"error in file",time_type])
+
+        elif(val==-2):
+            results.append([instance,val,time_type])
 
         else:
             results.append([instance,val,time_type])
             print(instance,val,time_type)
 
     df = pd.DataFrame(results, columns=['INSTANCE', 'TIME', 'TIME_TYPE'])
-        
     df.to_csv('results.csv', index=False) 
-
-
-    
-
-    
