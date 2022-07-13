1626806bc9bde478bb4504fe8fa4005e8df1bdc1
lute47lillo <lutelillocs47@gmail.com>
2022-07-13 15:46:45 -0500

    Unary parsing, location issue->fail

--- src/eclingo/parsing/transformers/ast_reify.py
+++ src/eclingo/parsing/transformers/ast_reify.py
@@ -8,29 +8,18 @@ from clingo.ast import (
 )
 from typing import List
 
-# TODO: In the case of -a(b,c)
-# We get a Unary operation first, therefore: parsed_lit.atom.symbol.argument -> a(b,c) and is a term from which we can do
-# parsed_lit.atom.symbol.argument.arguments[i] to get the arg_list. The operator_type will return the -
-# Type UnaryOperation
-
-
 def symbolic_literal_to_term(x: AST) -> AST:
     symbol = x.atom.symbol
     sign_name = refine_name(x.sign)
     
-    '''
-    # Commenting out for coverage passing
+    args: List[AST] = []
     if x.atom.symbol.ast_type == ASTType.UnaryOperation:
-        symbol = symbol.argument
-        # TODO: Create a helper function to deal with Unary Operation types
-    '''
+        return unary_parsing(x, sign_name, args)
     
     len_list = len(symbol.arguments)
-    
     # Create args list (Symbolic terms from the arguments list)
-    args: List[AST] = []
     if sign_name == 'not1' or sign_name == 'not2': # Base Case -> Negation
-    
+            
         if not len_list:  # Base Case 1. No Symbolic term arguments, then literal becomes only argument to not1/2 negation.
             lit_fun = [ast.SymbolicTerm(x.location, clingo.Function(symbol.name, [], True))]
             
@@ -40,7 +29,6 @@ def symbolic_literal_to_term(x: AST) -> AST:
                 args.append(arg)
             lit_fun = [ast.Function(x.location, symbol.name, args, False)]
         
-           
     elif len_list < 1:  # No arguments and no negation case -> Return basic Symbolic Term
         return ast.SymbolicTerm(x.location, clingo.Function(symbol.name, [], True))
     
@@ -53,6 +41,26 @@ def symbolic_literal_to_term(x: AST) -> AST:
         
     return ast.Function(x.location, sign_name, lit_fun, False)
 
+def unary_parsing(x: AST, sign_name: str, args: List[AST]):
+    symbol = x.atom.symbol
+    symbol = symbol.argument
+
+    n = len(symbol.arguments)
+    if n < 1:
+        n_arg = ast.SymbolicTerm(x.location, clingo.Function(symbol.name, [], True))
+        return ast.UnaryOperation(x.location, 0, n_arg)
+    
+    for t in range(n): 
+        arg = ast.SymbolicTerm(x.location, clingo.Function(str(symbol.arguments[t]), [], True))
+        args.append(arg)
+    lit_fun = ast.Function(x.location, symbol.name, args, False)
+    unary_term = ast.UnaryOperation(x.location, 0, lit_fun)
+    
+    if sign_name == 'not1' or sign_name == 'not2':
+            return ast.Function(x.location, sign_name, [unary_term], False)
+    
+    return unary_term
+
 # Helper Function to refine Negation names
 def refine_name(sign) -> str:
     name = ''
@@ -61,3 +69,5 @@ def refine_name(sign) -> str:
     elif (sign == Sign.DoubleNegation):
         name = 'not2'
     return name
+        
+        
--- tests/test_reification2.py
+++ tests/test_reification2.py
@@ -43,8 +43,6 @@ class Test(ASTTestCase):
         self.assertEqual(result, parsed_term)
 
     def test_symbolic_literal_to_term(self):
-        #self.assert_symbolic_literal_to_term("-a(b,c)","-a(b,c)")
-        
         self.assert_symbolic_literal_to_term("a(b)","a(b)")
         self.assert_symbolic_literal_to_term("a","a")
         self.assert_symbolic_literal_to_term("not a","not1(a)")
@@ -53,8 +51,10 @@ class Test(ASTTestCase):
         self.assert_symbolic_literal_to_term("a(b,c)","a(b,c)")
         self.assert_symbolic_literal_to_term("not a(b,c)","not1(a(b,c))")
         self.assert_symbolic_literal_to_term("not not a(b,c)","not2(a(b,c))")
-        '''
+        
+        self.assert_symbolic_literal_to_term("-a","-a")
+        self.assert_symbolic_literal_to_term("-a(b,c)","-a(b,c)")
         self.assert_symbolic_literal_to_term("not -a(b,c)","not1(-a(b,c))")
         self.assert_symbolic_literal_to_term("not not -a(b,c)","not2(-a(b,c))")
-        '''
+        
 
