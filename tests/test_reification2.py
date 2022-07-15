3b421852a5eb34e31b401cf8e971be2cf8448f83
jorgefandinno <jfandinno@unomaha.edu>
2022-07-15 04:47:50 -0500

    uncoment tests in test_symbolic_literal_to_term

--- tests/test_reification2.py
+++ tests/test_reification2.py
@@ -59,7 +59,7 @@ class Test(ASTTestCase):
         self.assert_symbolic_literal_to_term("not a(b,c)", "not1(a(b,c))")
         self.assert_symbolic_literal_to_term("not not a(b,c)", "not2(a(b,c))")
 
-        # self.assert_symbolic_literal_to_term("-a","-a")
-        # self.assert_symbolic_literal_to_term("-a(b,c)","-a(b,c)")
-        # self.assert_symbolic_literal_to_term("not -a(b,c)","not1(-a(b,c))")
-        # self.assert_symbolic_literal_to_term("not not -a(b,c)","not2(-a(b,c))")
+        self.assert_symbolic_literal_to_term("-a", "-a")
+        self.assert_symbolic_literal_to_term("-a(b,c)", "-a(b,c)")
+        self.assert_symbolic_literal_to_term("not -a(b,c)", "not1(-a(b,c))")
+        self.assert_symbolic_literal_to_term("not not -a(b,c)", "not2(-a(b,c))")
