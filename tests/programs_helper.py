bfd16bf683748a93f100f3e50b84c519a37ed38f
jorgefandinno <jfandinno@unomaha.edu>
2023-09-20 11:11:41 -0500

    refactor test_progarms

--- tests/programs_helper.py
+++ tests/programs_helper.py
@@ -1,13 +1,16 @@
-from typing import List, NamedTuple
+from typing import List, NamedTuple, Optional
 
 from eclingo.solver.candidate import Candidate
 
 
 class Program(NamedTuple):
-    program: str
-    non_ground_reification: str = None
-    candidates: List[Candidate] = None
-    wv_candidates: List[Candidate] = None
-    unoptimiced_candidates: List[Candidate] = None
-    ground_reification: str = None
+    program: Optional[str]
+    non_ground_reification: Optional[str] = None
+    ground_reification: Optional[str] = None
+    candidates_00_str: Optional[List[str]] = None
+    candidates_00: Optional[List[Candidate]] = None
+    candidates_01_str: Optional[List[str]] = None
+    candidates_01: Optional[List[Candidate]] = None
+    candidates_wv_str: Optional[List[str]] = None
+    candidates_wv: Optional[List[Candidate]] = None
     description: str = ""
