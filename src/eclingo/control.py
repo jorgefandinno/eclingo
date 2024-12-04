bba0db2590dc272de770dbc241f262cb961bde28
Jorge Fandinno <jorgefandinno@gmail.com>
2024-12-04 12:54:21 -0600

    typing control.py

--- src/eclingo/control.py
+++ src/eclingo/control.py
@@ -1,8 +1,9 @@
 import sys
 import time
-from typing import Iterable, Tuple
+from typing import Iterable, List, Optional, Sequence, Tuple
 
 from clingo import Symbol
+from clingo.ast import AST
 
 from eclingo.config import AppConfig
 from eclingo.grounder import Grounder
@@ -10,11 +11,11 @@ from eclingo.parsing import parser
 from eclingo.solver import SolverReification
 
 
-def parse_program(stm, parameters=None, name="base"):
+def parse_program(stm, parameters=None, name: str = "base") -> List[AST]:
     """Helping function to parse program for flag: --output-e=rewritten"""
     if parameters is None:
         parameters = []
-    ret = []
+    ret: List[AST] = []
     parser.parse_program(
         stm,
         ret.append,
@@ -26,13 +27,13 @@ def parse_program(stm, parameters=None, name="base"):
 
 
 class Control(object):
-    def __init__(self, control, config=None):
+    def __init__(self, control, config=None) -> None:
         # if control is not None:
         self.project = control.configuration.solve.project
         self.max_models = int(control.configuration.solve.models)
         control.configuration.solve.project = "auto,3"
         control.configuration.solve.models = 0
-        self.rewritten_program = []
+        self.rewritten_program: List[AST] = []
         self.control = control
         if config is None:
             config = AppConfig(semantics="c19-1")
@@ -44,32 +45,34 @@ class Control(object):
         self.grounder = Grounder(self.control, self.config)
         self.models = 0
         self.grounded = False
-        self.solver = None
-        self.grounding_time = 0
-        self.main_grounding_time = 0
-        self.solving_time = 0
+        self.solver: Optional[SolverReification] = None
+        self.grounding_time: float = 0
+        self.main_grounding_time: float = 0
+        self.solving_time: float = 0
 
-    def add_program(self, program):
+    def add_program(self, program) -> None:
         if self.config.eclingo_rewritten == "rewritten":
             self.rewritten_program.extend(parse_program(program))
         else:
             self.grounder.add_program(program)
 
-    def load(self, input_path):
+    def load(self, input_path) -> None:
         with open(input_path, "r") as program:
             self.add_program(program.read())
 
-    def ground(self, parts: Iterable[Tuple[str, Iterable[Symbol]]] = (("base", []),)):
+    def ground(
+        self, parts: Sequence[Tuple[str, Sequence[Symbol]]] = (("base", []),)
+    ) -> None:
         start_time = time.time()
         self.grounder.ground(parts)
         self.grounded = True
         self.main_grounding_time += time.time() - start_time
         self.grounding_time += self.main_grounding_time
 
-    def preprocess(self):
+    def preprocess(self) -> None:
         pass
 
-    def prepare_solver(self):
+    def prepare_solver(self) -> None:
         if not self.grounded:
             self.ground()
         start_time = time.time()
